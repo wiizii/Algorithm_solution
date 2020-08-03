e15324efab303e59583c4d9ae44d582f3ba611de
119wjw <119wjw@naver.com>
2020-08-04 01:36:42 +0900

    1005.cpp

---
+++ 1005.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int max(const int &a, const int &b){
+    if(a >= b)
+        return a;
+    else
+        return b;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        queue<int> q;
+        cin >> n >> k;
+        vector<int> adj[n+1];
+        vector<int> idx(n+1);
+        vector<int> T(n+1);
+        vector<int> ans(n+1,0);
+        for(int i=1;i<=n;i++){
+            int a;
+            cin >> a;
+            T[i] = ans[i] = a;
+        }
+        for(int i=0; i<k; i++){
+            int u,v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            idx[v]++;
+        }
+        for(int i=1;i<=n;i++)
+            if(idx[i] == 0)
+                q.push(i);
+        while(!q.empty()){
+            int x = q.front();
+            q.pop();
+            for(int y : adj[x]){
+                if(ans[y] < ans[x]+T[y])
+                    ans[y] = ans[x] + T[y];
+                idx[y]--;
+                if(idx[y] == 0)
+                    q.push(y);
+            }
+        }
+        int w;
+        cin >> w;
+        cout << ans[w] << "\n";
+    }
+    return 0;
+}
+
---
+++ 17140.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int A[100][100];
+int r,c,k;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin >> r >> c >> k;
+    vector<int> v;
+
+}
+
