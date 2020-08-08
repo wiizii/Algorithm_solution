4f9f14e1f22e6049888c7806c705eb7a8e5dc4b6
119wjw <56618964+119wjw@users.noreply.github.com>
2020-08-09 00:38:44 +0900

    Update 1774.cpp

--- 1774.cpp
+++ 1774.cpp
@@ -1,20 +1,69 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
-#include <queue>
+#include <algorithm>
 using namespace std;
 
-check
-
-struct POS{
-    int x;
-    int y;
+struct Pos {
+	long long x, y;
 };
 
-int main(){
-    int N, M;
-    cin >> N >> M;
-    POS pos[1001];
-    for(int i=0;i<M;i++){
-        
-    }
+struct Node {
+	int from, to;
+	double cost;
+};
+
+int parent[1001];
+vector<Pos> v(1001);
+vector<Node> adj;
+int n, m;
+
+int find(int u) {
+	if (parent[u] == u)
+		return u;
+	return parent[u] = find(parent[u]);
+}
+
+bool merge(int u, int v) {
+	u = find(u);
+	v = find(v);
+	if (u == v)
+		return false;
+	parent[u] = v;
+	return true;
+}
+
+double len(const Pos& a, const Pos& b) {
+	return sqrt(pow(a.x-b.x, 2) + pow(a.y-b.y, 2));
+}
+
+bool cmp(const Node& a, const Node& b) {
+	return a.cost < b.cost;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> v[i].x >> v[i].y;
+		parent[i] = i;
+	}
+	for (int i = 1; i <= n - 1; i++) 
+		for (int j = i + 1; j <= n; j++) 
+			adj.push_back({ i,j,len(v[i],v[j]) });
+
+	sort(adj.begin(), adj.end(), cmp);
+	double ans = 0;
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		merge(a, b);
+	}
+
+	for (int i = 0; i < adj.size(); i++)
+		if (merge(adj[i].from, adj[i].to))
+			ans += adj[i].cost;
+
+	printf("%.2lf\n", ans);
+	return 0;
 }
