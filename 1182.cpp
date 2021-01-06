9a225869784c3fea375da8a4feff7d32a7730251
119wjw <119wjw@naver.com>
2021-01-06 17:59:19 +0900

    1182.cpp

--- 1182.cpp
+++ 1182.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int n, s;
+int num[20];
 int ans = 0;
+int n, s;
 
 void solve(int cnt, int sum, int start) {
     if (cnt != 0 && sum == s) ans++;
     if (cnt == n) return;
-    for (int i = start; i < n; i++) {
-        solve(cnt + 1, sum + arr[i], i + 1);
-    }
+    for (int i = start; i < n; i++) solve(cnt + 1, sum + i, i + 1);
 }
 
 int main() {
     cin >> n >> s;
-    for (int i = 0; i < n; i++) cin >> arr[i];
+    for (int i = 0; i < n; i++) cin >> num[i];
     solve(0, 0, 0);
     cout << ans << "\n";
     return 0;
