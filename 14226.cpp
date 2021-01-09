36dec21966b6abadfce21a328171435d85cb8b23
119wjw <119wjw@naver.com>
2021-01-09 19:41:10 +0900

    14226.cpp

--- 14226.cpp
+++ 14226.cpp
@@ -1,24 +1,39 @@
-#include <algorithm>
 #include <iostream>
+#include <queue>
+#include <cstring>
 using namespace std;
-int dp[1001] = {0, 1};
 
-int solve(int s) {
-    if (s == 1) return 1;
-    if (dp[s]) return dp[s];
-    if (!(s & 1)) {
-        dp[s] = min(solve(s / 2) + 2, solve(s - 1) + 1);
-    } else {
-        dp[s] = solve(s - 1) + 1;
-    }
-    return dp[s];
-}
+int check[1001][1001];
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int s;
-    cin >> s;
-    cout << solve(s) << "\n";
-    return 0;
+	int s;
+	queue<pair<int, int>> q;
+	cin >> s;
+	q.push({ 1,0 });
+	memset(check, -1, sizeof(check));
+	check[1][0] = 0;
+
+	while (!q.empty()) {
+		int dist = q.front().first;
+		int clip = q.front().second;
+		q.pop();
+		if (dist == s) {
+			cout << check[dist][clip] << "\n";
+			break;
+		}
+		if (check[dist][dist] == -1) {
+			q.push({ dist,dist });
+			check[dist][dist] = check[dist][clip] + 1;
+		}
+		if (dist + clip < 1001 && check[dist + clip][clip] == -1) {
+			q.push({ dist + clip, clip });
+			check[dist + clip][clip] = check[dist][clip] + 1;
+		}
+		if (dist > 0 && check[dist - 1][clip] == -1) {
+			q.push({ dist - 1 , clip });
+			check[dist - 1][clip] = check[dist][clip] + 1;
+		}
+	}
+
+	return 0;
 }
