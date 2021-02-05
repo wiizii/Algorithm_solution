7ee0428536ad6adf41b1486a2bf6fe7f5b1e2c54
119wjw <119wjw@naver.com>
2021-02-05 18:01:55 +0900

    6087.cpp

--- 6087.cpp
+++ 6087.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 const int INF = 0x3f3f3f3f;
 string board[101];
-int check[101][101][4];
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1, 0, 0};
+int check[101][101];
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
 vector<pair<int, int>> C;
 
 int main() {
@@ -22,29 +22,48 @@ int main() {
         for (int j = 0; j < w; j++) {
             board[i][j] = s[j];
             if (board[i][j] == 'C') C.push_back({i, j});
-            for (int k = 0; k < 4; k++) check[i][j][k] = INF;
         }
     }
+    memset(check, -1, sizeof(check));
     queue<pair<pair<int, int>, int>> q;
     for (int i = 0; i < 4; i++) {
         q.push({{C[0].first, C[0].second}, i});
-        check[C[0].first][C[0].second][i] = 0;
+        check[C[0].first][C[0].second] = 0;
     }
     while (!q.empty()) {
         int x = q.front().first.first;
         int y = q.front().first.second;
         int d = q.front().second;
         q.pop();
+        if (x == C[1].first && y == C[1].second) {
+            ans = check[x][y];
+            break;
+        }
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (nx >= h || nx < 0 || ny >= w || ny < 0) continue;
-            if (board[nx][ny] == '*') continue;
-            if (i == d) {
-                check[nx][ny][i] = check[x][y][d];
-            } else
-                check[nx][ny][i] = check[x][y][d] + 1;
+            if (nx >= h || nx < 0 || ny >= w || ny < 0 || board[nx][ny] == '*')
+                continue;
+            if (check[nx][ny] == -1) {
+                if (d == i)
+                    check[nx][ny] = check[x][y];
+                else
+                    check[nx][ny] = check[x][y] + 1;
+                q.push({{nx, ny}, i});
+            } else {
+                if (d == i && check[nx][ny] > check[x][y])
+                    check[nx][ny] = check[x][y];
+                else if(d != i && check[nx][ny] > check[x][y]+1)
+                    check[nx][ny] = check[x][y] + 1;
+                q.push({{nx, ny}, i});
+            }
         }
+    }
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            cout << check[i][j] << "\t";
+        }
+        cout << "\n";
     }
     cout << ans << "\n";
 }
