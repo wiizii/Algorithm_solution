fd97a5fda102372ac049ac8649900e414e9d5e97
119wjw <119wjw@naver.com>
2021-02-14 03:42:52 +0900

    1450.cpp

--- 1450.cpp
+++ 1450.cpp
@@ -1,25 +1,32 @@
-#include <algorithm>
 #include <iostream>
-#define ll long long
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+int n, c;
 int arr[31];
 
 int main() {
-    int ans = 1;
-    int n, c;
-    cin >> n >> c;
-    for (int i = 0; i < n; i++) cin >> arr[i];
-    sort(arr, arr + n);
-    for (int i = 0; i < n; i++) {
-        int r = i + 1;
-        ll sum = arr[i];
-        if (sum <= c) ans++;
-        while (r < n) {
-            sum += arr[r++];
-            if (sum <= c) ans++;
-        }
-    }
+	cin >> n >> c;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	int half = n / 2;
+	int fsize = (1 << (n - half)), bsize = (1 << half);
+	vector<long long> front(fsize), back(bsize);
+	for (int i = 0; i < fsize; i++)
+		for (int j = 0; j < n - half; j++)
+			if (i & (1 << j)) front[i] += arr[j];
+	for (int i = 0; i < bsize; i++)
+		for (int j = 0; j < half; j++)
+			if (i & (1 << j)) back[i] += arr[j + (n - half)];
+	sort(front.begin(), front.end());
+	long long ans = 0;
+	for (int i = 0; i < back.size(); i++) {
+		long long& cur = back[i];
+		if(c >= cur)
+			ans += upper_bound(front.begin(), front.end(), c - cur) - front.begin();
+	}
+	cout << ans << "\n";
 
-    cout << ans << "\n";
+}
 }
