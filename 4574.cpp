53a9887a73e53851454f60e94a1d40948b1c17aa
119wjw <119wjw@naver.com>
2021-03-09 00:32:47 +0900

    4574.cpp

--- 4574.cpp
+++ 4574.cpp
@@ -7,18 +7,19 @@ int board[9][9];
 bool check[10][10];
 int dx[2] = {1, 0};
 int dy[2] = {0, 1};
+bool flag = false;
 
 void print() {
-    for (int i = 1; i <= 9; i++) {
-        for (int j = 1; j <= 9; j++) {
-            cout << board[i][j] << " ";
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << board[i][j];
         }
         cout << "\n";
     }
 }
 
 bool isPossible(int x, int y, int n) {
-    for (int i = 1; i <= 9; i++) {
+    for (int i = 0; i < 9; i++) {
         if (board[i][y] == n) return false;
         if (board[x][i] == n) return false;
     }
@@ -30,13 +31,16 @@ bool isPossible(int x, int y, int n) {
     return true;
 }
 
-bool solve(int n) {
+void solve(int n) {
     if (n == 81) {
         print();
-        return true;
+        flag = true;
+        return;
     }
     int x = n / 9, y = n % 9;
-    if (board[x][y] == 0) {
+    if (board[x][y] != 0) {
+        solve(n + 1);
+    } else {
         for (int k = 0; k < 2; k++) {
             int nx = x + dx[k];
             int ny = y + dy[k];
@@ -44,24 +48,22 @@ bool solve(int n) {
             if (board[nx][ny] != 0) continue;
             for (int i = 1; i <= 9; i++) {
                 for (int j = 1; j <= 9; j++) {
-                    if (i == j) continue;
-                    if (!check[i][j]) {
-                        if (isPossible(x, y, i) && isPossible(nx, ny, j)) {
-                            check[i][j] = true;
-                            board[x][y] = i;
-                            board[nx][ny] = j;
-                            if (solve(n + 1)) return true;
-                            board[x][y] = 0;
-                            board[nx][ny] = 0;
-                            check[i][j] = false;
-                        }
+                    if (i != j && !check[i][j] && !check[j][i] &&
+                        isPossible(x, y, i) && isPossible(nx, ny, j)) {
+                        check[i][j] = true;
+                        check[j][i] = true;
+                        board[x][y] = i;
+                        board[nx][ny] = j;
+                        if (!flag) solve(n + 1);
+                        board[x][y] = 0;
+                        board[nx][ny] = 0;
+                        check[i][j] = false;
+                        check[j][i] = false;
                     }
                 }
             }
         }
-    } else
-        solve(n + 1);
-    return false;
+    }
 }
 
 int main() {
@@ -74,13 +76,18 @@ int main() {
         if (n == 0) break;
         memset(board, 0, sizeof(board));
         memset(check, 0, sizeof(check));
+        flag = false;
         for (int i = 0; i < n; i++) {
             int x;
+            int a, b;
             string s;
             cin >> x >> s;
+            a = x;
             board[s[0] - 'A'][s[1] - '1'] = x;
             cin >> x >> s;
+            b = x;
             board[s[0] - 'A'][s[1] - '1'] = x;
+            check[a][b] = check[b][a] = true;
         }
         for (int i = 1; i <= 9; i++) {
             string s;
