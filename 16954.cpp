498bfbf7d29003b18ff8ac5a2426a900475741cb
119wjw <119wjw@naver.com>
2021-03-15 11:45:43 +0900

    16954.cpp

--- 16954.cpp
+++ 16954.cpp
@@ -1,43 +1,40 @@
 #include <iostream>
 #include <queue>
+#include <tuple>
 #include <vector>
 using namespace std;
+
 char board[8][8];
-bool check[8][8];
-vector<pair<int, int>> wall;
-
-void wall_move() {
-    for (int i = 0; i < wall.size(); i++) {
-        int &x = wall[i].first;
-        int &y = wall[i].second;
-        if (x < 8) {
-            board[x][y] = '.';
-            x++;
-            if (x != 8) board[x][y] = '#';
-        }
-    }
-}
-
-void print() {
-    cout << "\n";
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            cout << board[i][j];
-        }
-        cout << "\n";
-    }
-}
+bool check[8][8][9];
+int dx[] = {1, 1, 1, 0, 0, 0, -1, -1, -1};
+int dy[] = {1, 0, -1, 1, 0, -1, 1, 0, -1};
 
 int main() {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            cin >> board[i][j];
-            if (board[i][j] == '#') wall.push_back({i, j});
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++) cin >> board[i][j];
+    queue<tuple<int, int, int>> q;
+    check[7][0][0] = true;
+    q.push({7, 0, 0});
+    while (!q.empty()) {
+        int x, y, t;
+        tie(x, y, t) = q.front();
+        q.pop();
+        if (x == 0 && y == 7) {
+            cout << 1 << "\n";
+            return 0;
+        }
+        for (int i = 0; i < 9; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            int nt = min(t + 1, 8);
+            if (nx < 0 || ny < 0 || nx >= 8 || ny >= 8) continue;
+            if (nx - t >= 0 && board[nx - t][ny] == '#') continue;
+            if (nx - t - 1 >= 0 && board[nx - t - 1][ny] == '#') continue;
+            if (!check[nx][ny][nt]) {
+                check[nx][ny][nt] = true;
+                q.push({nx, ny, nt});
+            }
         }
     }
-    queue<pair<int, int>> q;
-    q.push({7, 0});
-    check[7][0] = true;
-    while (!q.empty()) {
-        }
+    cout << 0 << "\n";
 }
