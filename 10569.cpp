29b4de3956e5f0fff5f6d8100c090650543f7fbd
119wjw <119wjw@naver.com>
2021-03-19 09:15:02 +0900

    10569.cpp

---
+++ 10569.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int v, e;
+        scanf("%d %d", &v, &e);
+        printf("%d\n", 2 - v + e);
+    }
+}
