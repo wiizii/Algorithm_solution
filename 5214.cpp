0f746eb4b2f77b63b8a543a9c61221a00756a765
119wjw <119wjw@naver.com>
2021-04-11 19:17:50 +0900

    5214.cpp

--- 5214.cpp
+++ 5214.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 100001;
+const int MAX = 102000;
 
 int n, k, m;
 vector<int> adj[MAX];
-vector<int> hyper[1001];
 int check[MAX];
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> k >> m;
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < k; j++) {
+    if (n == 1) {
+        cout << 1 << "\n";
+        return 0;
+    }
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= k; j++) {
             int x;
             cin >> x;
-            adj[x].push_back(i);
-            hyper[i].push_back(x);
+            adj[x].push_back(n + i);
+            adj[n + i].push_back(x);
         }
     }
     queue<int> q;
@@ -26,16 +29,17 @@ int main() {
         int cur = q.front();
         q.pop();
         for (int i = 0; i < adj[cur].size(); i++) {
-            for (int j = 0; j < hyper[adj[cur][i]].size(); j++) {
-                int next = hyper[adj[cur][i]][j];
-                if (!check[next]) {
+            int next = adj[cur][i];
+            if (!check[next]) {
+                if (next <= n)
                     check[next] = check[cur] + 1;
-                    if (next == n) {
-                        cout << check[next] << "\n";
-                        return 0;
-                    }
-                    q.push(next);
+                else
+                    check[next] = check[cur];
+                if (next == n) {
+                    cout << check[next] << "\n";
+                    return 0;
                 }
+                q.push(next);
             }
         }
     }
