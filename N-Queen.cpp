63205675f641d304753f93b88551488b092ce15d
119wjw <119wjw@naver.com>
2021-05-24 09:24:58 +0900

    N-Queen.cpp

--- N-Queen.cpp
+++ N-Queen.cpp
@@ -1,30 +1,29 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
+
 using namespace std;
 int check[15];
 int N;
 int cnt_Queen = 0;
 
-bool isSameline(int x){
-    for(int i=0;i<x;i++)
-        if(check[i] == check[x] || abs(check[i]-check[x]) == x-i)
-            return 1;
+bool isSameline(int x) {
+    for (int i = 0; i < x; i++)
+        if (check[i] == check[x] || abs(check[i] - check[x]) == x - i) return 1;
     return 0;
 }
 
-void bt(int cnt){
-    if(cnt==N){
+void bt(int cnt) {
+    if (cnt == N) {
         cnt_Queen++;
         return;
     }
-    for(int i=0;i<N;i++){
+    for (int i = 0; i < N; i++) {
         check[cnt] = i;
-        if(!isSameline(cnt))
-            bt(cnt+1);
+        if (!isSameline(cnt)) bt(cnt + 1);
     }
 }
 
-int main(){
+int main() {
     cin >> N;
     bt(0);
     cout << cnt_Queen << "\n";
