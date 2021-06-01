d566b5323d67b1fc50098e60bfbc4a5d9d168e22
119wjw <119wjw@naver.com>
2021-06-01 09:33:04 +0900

    15486.cpp

--- 15486.cpp
+++ 15486.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
-#define pii pair<int, int>
 using namespace std;
+using pii = pair<int, int>;
 
-pii arr[1500001];
-int dp[1500001];
+pii arr[1500111];
+int dp[1500111];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -11,13 +11,12 @@ int main() {
     int ans = 0;
     int n;
     cin >> n;
-    for (int i = 0; i < n; i++) cin >> arr[i].first >> arr[i].second;
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) cin >> arr[i].first >> arr[i].second;
+    for (int i = 1; i <= n; i++) {
         auto [t, p] = arr[i];
-        if (i + t > n) continue;
         dp[i] = max(dp[i], dp[i - 1]);
         dp[i + t] = max(dp[i + t], dp[i] + p);
-        ans = max(ans, dp[i + t]);
     }
+    for (int i = 1; i <= n + 1; i++) ans = max(ans, dp[i]);
     cout << ans << "\n";
 }
