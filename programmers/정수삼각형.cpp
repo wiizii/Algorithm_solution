8b4f000774f9cba8543fe0bfa66b76968c2bd463
119wjw <119wjw@naver.com>
2021-06-15 19:06:27 +0900

    정수삼각형.cpp

---
+++ programmers/정수삼각형.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    int dp[501][501] = {
+        0,
+    };
+    dp[0][0] = triangle[0][0];
+    int height = triangle.size();
+    for (int i = 1; i < height; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (j == 0)
+                dp[i][j] = dp[i - 1][j] + triangle[i][j];
+            else if (j == i)
+                dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+            else
+                dp[i][j] = max(dp[i - 1][j - 1] + triangle[i][j],
+                               dp[i - 1][j] + triangle[i][j]);
+        }
+    }
+    for (int i = 0; i < height; i++) {
+        answer = max(answer, dp[height - 1][i]);
+    }
+    return answer;
+}
