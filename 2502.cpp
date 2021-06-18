e18aecc7901957dc32db0205afba6cd6d763ddf6
119wjw <119wjw@naver.com>
2021-06-18 13:17:08 +0900

    2502.cpp

--- 2502.cpp
+++ 2502.cpp
@@ -1,23 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int solve(int a, int b, int n){
-  if(n == 1)
-    return a;
-  if(n == 2)
-    return b;
-  return solve(a,b,n-1) + solve(a,b,n-2);
+int dp[31];
+
+int solve(int a, int b, int n) {
+    if (dp[n]) return dp[n];
+    if (n == 1) return a;
+    if (n == 2) return b;
+    return dp[n] = solve(a, b, n - 1) + solve(a, b, n - 2);
 }
 
-int main(){
-  int d,k;
-  cin >> d >> k;
-  for(int i=0;i<4000;i++){
-    for(int j=i;j<4000;j++){
-      if(solve(i,j,d) == k){
-        cout << i << "\n" << j << "\n";
-        break;
-      }
+int main() {
+    int d, k;
+    cin >> d >> k;
+    for (int i = 1; i < 100000; i++) {
+        for (int j = i; j < 100000; j++) {
+            memset(dp, 0, sizeof(dp));
+            if (solve(i, j, d) == k) {
+                cout << i << "\n" << j << "\n";
+                return 0;
+            }
+        }
     }
-  }
 }
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int arr[8];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+}
