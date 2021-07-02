73769be11bb66c4c3e9881ba23e9e2d483b7c7a6
119wjw <119wjw@naver.com>
2021-07-02 20:16:21 +0900

    7869.cpp

--- 7869.cpp
+++ 7869.cpp
@@ -1,29 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double heron(double a, double b, double c) {
-    double s = (a + b + c) / 2;
-    return sqrt(s * (s - a) * (s - b) * (s - c));
+double get_dist(double x1, double y1, double x2, double y2) {
+    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
-double getArea(double r, double theta) {
-    return pow(r, 2) * theta - pow(r, 2) * sin(theta) * cos(theta);
+double get_area(double r, double theta) {
+    return (r * r * 2 * theta) / 2 - r * r * sin(2 * theta) / 2;
 }
 
 int main() {
     double x1, y1, r1, x2, y2, r2;
+    double theta1, theta2;
+    double area = 0;
     cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
 
-    double r3 = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
-    double size = heron(r1, r2, r3);
+    if (r1 < r2) {
+        swap(x1, x2);
+        swap(y1, y2);
+        swap(r1, r2);
+    }
 
-    double theta1 = asin((2 * size) / (r1 * r3));
-    double theta2 = asin((2 * size) / (r2 * r3));
+    double d = get_dist(x1, y1, x2, y2);
 
-    double area1 = getArea(r1, theta1);
-    double area2 = getArea(r2, theta2);
-
+    if (r1 - r2 < d && d < r1 + r2) {
+        theta1 = acos((r1 * r1 + d * d - r2 * r2) / (2 * r1 * d));
+        theta2 = acos((r2 * r2 + d * d - r1 * r1) / (2 * r2 * d));
+        area = get_area(r1, theta1) + get_area(r2, theta2);
+    } else if (d <= r1 - r2) {
+        area = r2 * r2 * M_PI;
+    }
     cout << fixed;
     cout.precision(3);
-    cout << area1 + area2 << "\n";
+    cout << area << "\n";
 }
