495a0611f8083ef46604b7b4c4134ad83e20b102
119wjw <119wjw@naver.com>
2021-07-07 22:39:42 +0900

    binary_plus.cpp

--- binary_plus.cpp
+++ binary_plus.cpp
@@ -2,28 +2,26 @@
 using namespace std;
 
 int main() {
-    string a, b;
-    cin >> a >> b;
-    reverse(a.begin(), a.end());
-    reverse(b.begin(), b.end());
-    if (a.size() < b.size()) swap(a, b);
-    for (int i = 0; i < b.size(); i++) a[i] += b[i] - '0';
-    for (int i = 0; i < a.size() - 1; i++) {
-        int k = a[i] - '0';
-        a[i] = (k % 2) + '0';
-        a[i + 1] += (k / 2);
+    int t;
+    cin >> t;
+    while (t--) {
+        string a, b;
+        bool chk[26];
+        memset(chk, 0, sizeof(chk));
+
+        cin >> a >> b;
+        for (int i = 0; i < a.size(); i++) chk[a[i] - 'a'] = true;
+        bool flag = true;
+        if ()
+            for (int i = 0; i < b.size(); i++) {
+                if (!chk[b[i] - 'a']) {
+                    flag = false;
+                    break;
+                }
+            }
+        if (flag)
+            cout << a << " & " << b << " are anagrams.\n";
+        else
+            cout << a << " & " << b << " are NOT anagrams.\n";
     }
-    if (a.back() == '2') {
-        a.back() = '0';
-        a.push_back('1');
-    } else if (a.back() == '3') {
-        a.back() = '1';
-        a.push_back('1');
-    }
-    while (!a.empty() && a.back() == '0') {
-        a.pop_back();
-    }
-    if (a.empty()) a.push_back('0');
-    reverse(a.begin(), a.end());
-    cout << a << "\n";
 }
