f4e3167e6755acee19210e8b0b2e9388c12a5a5d
119wjw <119wjw@naver.com>
2021-07-23 10:28:55 +0900

    6593.cpp

--- 6593.cpp
+++ 6593.cpp
@@ -11,7 +11,32 @@ int dz[] = {0, 0, 0, 0, 1, -1};
 tiii START, END;
 int n, m, o;
 
-int bfs() { memset(check, 0, sizeof(check)); }
+int bfs() {
+    memset(check, -1, sizeof(check));
+    queue<tiii> q;
+    q.push(START);
+    auto [a, b, c] = START;
+    check[a][b][c] = 0;
+    while (!q.empty()) {
+        auto [x, y, z] = q.front();
+        q.pop();
+        for (int i = 0; i < 6; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            int nz = z + dz[i];
+            if (nx < 0 || ny < 0 || nz < 0 || nx >= n || ny >= m || nz >= o)
+                continue;
+            if (check[nx][ny][nz] == -1 && arr[nx][ny][nz] != '#') {
+                check[nx][ny][nz] = check[x][y][z] + 1;
+                if (arr[nx][ny][nz] == 'E') {
+                    return check[nx][ny][nz];
+                }
+                q.push({nx, ny, nz});
+            }
+        }
+    }
+    return -1;
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
