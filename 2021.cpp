b7c3bfa79ccdad3f4e1b76c6dfa65942f4e6e18f
119wjw <119wjw@naver.com>
2021-08-17 19:43:55 +0900

    2021.cpp

--- 2021.cpp
+++ 2021.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+using pii = pair<int, int>;
 
 const int MAX = 200010;
 int n, l;
@@ -10,46 +9,43 @@ vector<int> adj[MAX];
 bool check[MAX];
 
 int bfs() {
-	int ret = 0;
-	queue<int> q;
-	q.push(start);
-	check[start] = true;
-	while (!q.empty()) {
-		int cur = q.front();
-		q.pop();
-		if (cur == last) 
-			return ret-1;
-		for (auto next : adj[cur]) {
-			if (!check[next]) {
-				if (next > n)
-					ret++;
-				check[next] = true;
-				q.push(next);
-			}
-		}
-	}
-	return -1;
+    queue<pii> q;
+    q.push({start, 0});
+    check[start] = true;
+    while (!q.empty()) {
+        auto [cur, cnt] = q.front();
+        q.pop();
+        if (cur == last) return cnt - 1;
+        for (auto next : adj[cur]) {
+            if (!check[next]) {
+                check[next] = true;
+                if (next > n)
+                    q.push({next, cnt});
+                else
+                    q.push({next, cnt + 1});
+            }
+        }
+    }
+    return -1;
 }
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> l;
-	for (int i = n+1; i <= n+l; i++) {
-		while (1) {
-			int x;
-			cin >> x;
-			if (x == -1)
-				break;
-			adj[i].push_back(x);
-			adj[x].push_back(i);
-		}
-	}
-	cin >> start >> last;
-	if (start == last) {
-		cout << 0 << "\n";
-		return 0;
-	}
-	cout << bfs() << "\n";
-
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> l;
+    for (int i = 1; i <= l; i++) {
+        while (1) {
+            int x;
+            cin >> x;
+            if (x == -1) break;
+            adj[n + i].push_back(x);
+            adj[x].push_back(n + i);
+        }
+    }
+    cin >> start >> last;
+    if (start == last) {
+        cout << 0 << "\n";
+        return 0;
+    }
+    cout << bfs() << "\n";
 }
