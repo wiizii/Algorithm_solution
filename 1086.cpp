a5066baaf68708b67a31b275aebb83667b9c8176
119wjw <119wjw@naver.com>
2021-09-22 20:15:30 +0900

    1086.cpp

--- 1086.cpp
+++ 1086.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-int arr[15];
+string arr[15];
 
-int dp[100][1 << 15];
+int dp[101][1 << 15];
 int n, k;
+int tenPowMod[51];
 
 int solve(int x, int visited) {
     if (visited == (1 << n) - 1) return 0;
@@ -17,9 +19,22 @@ int solve(int x, int visited) {
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     memset(dp, -1, sizeof(dp));
     cin >> n;
-    for (int i = 0; i < n; i++) cin >> arr[i];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
     cin >> k;
-    cout << solve(0, 0);
+    tenPowMod[0] = 1;
+    for (int i = 1; i < 51; i++) {
+        tenPowMod[i] = (tenPowMod[i - 1] * 10) % k;
+    }
+
+    ll a = 1;
+    for (int i = 1; i <= n; i++) a *= i;
+    ll b = solve(0, 0);
+    ll g = gcd(a, b);
+    cout << b / g << "/" << a / g << "\n";
 }
