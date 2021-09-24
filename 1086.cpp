15546679076b880f4c7975134aaa285c7d3b424a
119wjw <119wjw@naver.com>
2021-09-24 20:27:06 +0900

    1086.cpp

--- 1086.cpp
+++ 1086.cpp
@@ -2,20 +2,24 @@
 #define ll long long
 using namespace std;
 
-string arr[15];
+string s[15];
 
-int dp[101][1 << 15];
+ll arr[15][2];
+ll dp[1 << 15][101];
 int n, k;
-int tenPowMod[51];
+ll tenPowMod[55];
 
-int solve(int x, int visited) {
-    if (visited == (1 << n) - 1) return 0;
-    int &ret = dp[x][visited];
+ll solve(int visited, int x) {
+    if (visited == (1 << n) - 1) return (x == 0);
+    ll &ret = dp[visited][x];
     if (ret != -1) return ret;
+    ret = 0;
     for (int i = 0; i < n; i++) {
         if (visited & (1 << i)) continue;
-        ret = (solve(x + 1, visited | (1 << i)) + arr[i]);
+        int next = (x * tenPowMod[arr[i][1]] + arr[i][0]) % k;
+        ret += solve(visited | (1 << i), next);
     }
+    return ret;
 }
 
 int main() {
@@ -24,11 +28,17 @@ int main() {
     memset(dp, -1, sizeof(dp));
     cin >> n;
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> s[i];
     }
     cin >> k;
+    for (int i = 0; i < n; i++) {
+        arr[i][1] = s[i].size();
+        for (int j = 0; j < s[i].size(); j++) {
+            arr[i][0] = (arr[i][0] * 10 + (s[i][j] - '0')) % k;
+        }
+    }
     tenPowMod[0] = 1;
-    for (int i = 1; i < 51; i++) {
+    for (int i = 1; i < 55; i++) {
         tenPowMod[i] = (tenPowMod[i - 1] * 10) % k;
     }
 
