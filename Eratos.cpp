8416ce5d7152808565d18d998db6a2448253e3df
119wjw <119wjw@naver.com>
2021-10-12 09:36:50 +0900

    Eratos.cpp

--- Eratos.cpp
+++ Eratos.cpp
@@ -1,9 +1,16 @@
-void Eratos(int n) {
-    if (n <= 1) return;
-    bool* PrimeArray = new bool[n + 1];
-    for (int i = 2; i <= n; i++) PrimeArray[i] = true;
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 1000001;
+
+bool isPrime[MAX];
+void eratos(int n) {
+    for (int i = 2; i <= n; i++) isPrime[i] = true;
     for (int i = 2; i * i <= n; i++) {
-        if (PrimeArray[i])
-            for (int j = i * i; j <= n; j += i) PrimeArray[j] = false;
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
     }
 }
