766dc67e17e9868c4539842b6e37b436415cc388
119wjw <119wjw@naver.com>
2021-10-17 20:15:24 +0900

    17626.cpp

--- 17626.cpp
+++ 17626.cpp
@@ -1,29 +1,24 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int dp[50001] = {
-    0,
-    1,
-};
-vector<int> square;
+const int INF = 0x3f3f3f3f;
 
+int dp[50001];
+
+int n;
 int main() {
-    int n;
     cin >> n;
-    for (int i = 1; i * i <= 50000; i++) {
-        square.push_back(i * i);
-    }
-    for (int i = 2; i <= n; i++) {
-        int min_num = 0x3f3f3f3f;
-        for (int j = 0; square[j] <= i; j++) {
-            int tmp = i - square[j];
-            min_num = min(min_num, dp[tmp]);
-        }
-        dp[i] = min_num + 1;
+    for (int i = 1; i <= n; i++) {
+        if ((int)sqrt(i) * (int)sqrt(i) == i)
+            dp[i] = 1;
+        else
+            dp[i] = INF;
     }
 
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= (int)sqrt(i); j++) {
+            dp[i] = min(dp[i], dp[j * j] + dp[i - j * j]);
+        }
+    }
     cout << dp[n] << "\n";
-    return 0;
 }
