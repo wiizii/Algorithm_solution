ec15451d42ca3d1b730369f5e9c64420e933730f
119wjw <119wjw@naver.com>
2022-01-28 20:10:54 +0900

    구명보트.cpp

---
+++ programmers/구명보트.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    sort(people.begin(), people.end());
+    int i = 0, j = people.size() - 1;
+    while (i <= j) {
+        if (i != j && people[i] + people[j] <= limit) {
+            i++;
+            j--;
+        } else {
+            j--;
+        }
+        answer++;
+    }
+    return answer;
+}
