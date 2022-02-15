416622ebceaf61244f90541915da1fbb4dd05ed7
119wjw <119wjw@naver.com>
2022-02-15 16:29:30 +0900

    1806.cpp

--- 1806.cpp
+++ 1806.cpp
@@ -1,31 +1,37 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int arr[100000];
+const int MAX = 100001;
+const int INF = 0x3f3f3f3f;
+
+int arr[MAX];
 
 int main() {
-	int n,s;
-	int start = 0, end = 1;
-	cin >> n >> s;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-	int sum = arr[start] + arr[end];
-	int answer = 0x3f3f3f3f;
-	while (start <= end && end < n) {
-		if (s <= sum) 
-			answer = min(answer, end - start + 1);
-		if (start <= end && s <= sum) {
-			sum -= arr[start];
-			start++;
-		}
-		else if (end < n && s > sum) {
-			end++;
-			sum += arr[end];
-		}
-	}
-	if (answer == 0x3f3f3f3f) 
-		cout << 0 << "\n";
-	else
-		cout << answer << "\n";
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, s;
+    cin >> n >> s;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int l = 0, r = 0;
+    int acc = arr[r];
+    int ans = INF;
+    while (r < n) {
+        if (acc < s) {
+            acc += arr[++r];
+        } else if (acc == s) {
+            ans = min(ans, r - l + 1);
+            acc += arr[++r];
+        } else {
+            ans = min(ans, r - l + 1);
+            acc -= arr[l++];
+        }
+    }
+    if (ans == INF)
+        cout << 0 << endl;
+    else {
+        cout << ans << "\n";
+    }
 }
